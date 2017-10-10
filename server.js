3af49432a4fc78517c7ab310233f5c1ef366709c
Jacob Morton <mkoopajr@Jacobs-MacBook.local>
2017-10-09 21:16:32 -0500

    Added CORS and upped TTL.

--- server.js
+++ server.js
@@ -3,11 +3,24 @@
 const Hapi = require('hapi');
 const routes = require('./lib/routes');
 
+// Better way to do this?
+const urls = process.env.CORSURLS.split(' ');
+
 const server = new Hapi.Server();
-server.connection({host: '0.0.0.0', port: parseInt(process.env.PORT, 10) || 8900});
+server.connection({
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT, 10) || 8900,
+    routes: {
+        cors: {
+            origin: urls,
+            credentials: true
+        }
+    }
+});
 
 server.state('session', {
-    ttl: 60000,
+    // Two Hours, should be more than enough time.
+    ttl: 7200000,
     isSecure: true,
     isHttpOnly: true,
     encoding: 'base64json'
